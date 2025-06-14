486bd5188ca7d3ffec2fc7097d17ca08be933c18
Vladislav <kaknokote@gmail.com>
2025-06-14 22:27:40 +0300

    vite-cfg changed

--- visavi-client/vite.config.js
+++ visavi-client/vite.config.js
@@ -7,7 +7,10 @@ export default defineConfig({
 	plugins: [react(), tailwindcss()],
 	server: {
 		proxy: {
-			'/api': 'http://localhost:5000',
+			'/api': {
+				target: 'http://server:5000',
+				changeOrigin: true,
+			},
 		},
 	},
 });
